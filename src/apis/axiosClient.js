4d8b5cf9bc6fcea6a8df4b11cf295974a99595eb
hoangnghia0107 <hoangnghiahp0107@gmail.com>
2023-06-05 13:41:40 +0700

    05-06

---
+++ src/apis/axiosClient.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+
+const axiosClient = axios.create({
+    baseURL:"https://airbnbnew.cybersoft.edu.vn/api",
+    headers:{
+        TokenCybersoft:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA0MiIsIkhldEhhblN0cmluZyI6IjEwLzEwLzIwMjMiLCJIZXRIYW5UaW1lIjoiMTY5Njg5NjAwMDAwMCIsIm5iZiI6MTY2NzA2MjgwMCwiZXhwIjoxNjk3MDQzNjAwfQ.g_aUM-jnWQ1i_eCbjNfvNxudUdUPpfC36068g5o9Ung"
+    },
+})
+
+export default axiosClient
