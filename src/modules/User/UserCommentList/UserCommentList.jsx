79635e3f304030f83943cbb1f46838a56d7be892
hoangnghia0107 <hoangnghiahp0107@gmail.com>
2023-06-24 13:06:46 +0700

    xyz

--- src/modules/User/UserCommentList/UserCommentList.jsx
+++ src/modules/User/UserCommentList/UserCommentList.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { Container } from "react-bootstrap";
 import { getCommentList } from "../../../slices/commentListSlice";
 import "./UserCommentList.scss";
 import swal from "sweetalert";
@@ -14,6 +15,7 @@ function UserCommentList() {
   );
   const [commentList, setCommentList] = useState(null);
   const [isL, setIsL] = useState(false);
+
   useEffect(() => {
     setIsL(true);
     const fetch = async () => {
@@ -38,70 +40,66 @@ function UserCommentList() {
     );
 
   return (
-    <div className="commentManagement">
-      <h2>Danh sách bình luận của bạn</h2>
+    <>
 
-      <div className="body">
-        <div className="container">
-          <div className="row">
-            <table className="table">
-              <thead>
-                <tr className="th1">
-                  <th scope="col">#</th>
-                  <th scope="col">ID</th>
-                  <th scope="col">Mã phòng</th>
-                  {/* <th scope="col">Mã người dùng</th> */}
-                  <th scope="col">Ngày bình luận</th>
-                  <th scope="col">Nội dung</th>
-                  <th scope="col">Số sao đánh giá</th>
-                  <th scope="col">Actions</th>
-                </tr>
-              </thead>
-              <tbody>
-                {commentList?.map((item, index) => {
-                  const ngay = new Date(item.ngayBinhLuan);
-                  return (
-                    <tr key={index} className="th2">
-                      <th>{index + 1}</th>
-                      <td>{item.id}</td>
-                      <td>{item.maPhong}</td>
-                      {/* <td>{item.maNguoiBinhLuan}</td> */}
-                      <td>{`${
-                        ngay.getDate() < 10
-                          ? "0" + ngay.getDate()
-                          : ngay.getDate()
-                      }/${
-                        ngay.getMonth() + 1 < 10
-                          ? "0" + (ngay.getMonth() + 1)
-                          : ngay.getMonth() + 1
-                      }/${ngay.getFullYear()}`}</td>
-                      <td>{item.noiDung}</td>
-                      <td>{item.saoBinhLuan}</td>
-                      <td>
-                        {/* <button
-                        className="btn text-secondary me-1 border-warning mt-1"
-                        onClick={() => handleUpdateComment(index)}
-                      >
-                        <i className="bi bi-pencil-square"></i>
-                      </button> */}
-                        <button
-                          className="btn text-danger border-success mt-1"
-                          onClick={() =>
-                            navigate(`/phong-thue/${item.maPhong}`)
-                          }
-                        >
-                          <i class="bi bi-box-arrow-right"></i>
-                        </button>
-                      </td>
+      <div>
+        <Container>
+          <h2 className="tieuDeBL">Danh sách bình luận của bạn</h2>
+          <div className="body">
+            <div className="container">
+              <div className="row">
+                <table className="table">
+                  <thead>
+                    <tr className="th1">
+                      <th scope="col">#</th>
+                      <th scope="col">ID</th>
+                      <th scope="col">Mã phòng</th>
+                      <th scope="col">Ngày bình luận</th>
+                      <th scope="col">Nội dung</th>
+                      <th scope="col">Số sao đánh giá</th>
+                      <th scope="col">Actions</th>
                     </tr>
-                  );
-                })}
-              </tbody>
-            </table>
+                  </thead>
+                  <tbody>
+                    {commentList?.map((item, index) => {
+                      const ngay = new Date(item.ngayBinhLuan);
+                      return (
+                        <tr key={index} className="th2">
+                          <th>{index + 1}</th>
+                          <td>{item.id}</td>
+                          <td>{item.maPhong}</td>
+                          <td>{`${
+                            ngay.getDate() < 10
+                              ? "0" + ngay.getDate()
+                              : ngay.getDate()
+                          }/${
+                            ngay.getMonth() + 1 < 10
+                              ? "0" + (ngay.getMonth() + 1)
+                              : ngay.getMonth() + 1
+                          }/${ngay.getFullYear()}`}</td>
+                          <td>{item.noiDung}</td>
+                          <td>{item.saoBinhLuan}</td>
+                          <td>
+                            <button
+                              className="btn text-danger border-success mt-1"
+                              onClick={() =>
+                                navigate(`/phong-thue/${item.maPhong}`)
+                              }
+                            >
+                              <i class="bi bi-box-arrow-right"></i>
+                            </button>
+                          </td>
+                        </tr>
+                      );
+                    })}
+                  </tbody>
+                </table>
+              </div>
+            </div>
           </div>
-        </div>
+        </Container>
       </div>
-    </div>
+    </>
   );
 }
 
