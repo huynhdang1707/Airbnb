3f3162989c14d8c51bba8d52e97e9def166cacb6
hiim461 <lqhoang96@gmail.com>
2023-06-24 13:39:27 +0700

    246-1

--- src/modules/Admin/AdminHeader/AdminHeader.jsx
+++ src/modules/Admin/AdminHeader/AdminHeader.jsx
@@ -2,18 +2,18 @@ import React from "react";
 import "./AdminHeader.scss";
 
 function AdminHeader({ Toggle, toggle }) {
-  if (toggle) {
-    return (
-      <nav className="navbar navbar-expand-sm navbar-white bg-white px-3">
-        <i
-          className="navbar-brand bi bi-justify-left fs-4"
-          onClick={Toggle}
-        ></i>
-      </nav>
-    );
-  } else {
-    return null;
-  }
-}
+  // if (toggle) {
+    // return (
+    //   <nav className="navbar navbar-expand-sm navbar-white bg-white px-3">
+    //     <i
+    //       className="navbar-brand bi bi-justify-left fs-4"
+    //       onClick={Toggle}
+    //     ></i>
+    //   </nav>
+    // );
+//   } else {
+//     return null;
+//   }
+    }
 
 export default AdminHeader;
